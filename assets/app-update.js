924aa781170768448e3a0b05c0a223dbfcd00a4d
mbarreram <114419513+mbarreram@users.noreply.github.com>
2026-07-28 10:55:58 -0400

    Align updater with polished desktop shell release

--- assets/app-update.js
+++ assets/app-update.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  const APP_VERSION='2026.07.28.1';
+  const APP_VERSION='2026.07.28.2';
 
   function loadMiCursoXBrand(){
     if(!document.querySelector('link[data-micursox-brand]')){
